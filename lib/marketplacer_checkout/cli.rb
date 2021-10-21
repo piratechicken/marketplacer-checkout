b2d4e9adfac7dfaaeb1a68552de5142705e614bb
Simon Dwyer <simon@discolabs.com>
2021-10-21 18:10:49 +1100

    Rush a cli

--- lib/marketplacer_checkout/cli.rb
+++ lib/marketplacer_checkout/cli.rb
@@ -2,18 +2,46 @@
 
 module MarketplacerCheckout
   class CLI
-    def initialize(products_file_path)
+    def initialize(products_file_path, discounts_file_path)
       @products = MarketplacerCheckout::Product.load_all(products_file_path)
-      @cart = MarketplacerCheckout::Cart.new
+      available_discounts = MarketplacerCheckout::Discount.load_all(discounts_file_path)
+      @cart = MarketplacerCheckout::Cart.new(available_discounts)
     end
 
     def run
+      loop do
+        print_status
+
+        user_input = gets.strip
+
+        case user_input
+        when 'x'
+          puts "\nThanks for shopping with Marketplacer!\n"
+          abort
+        else
+          find_and_add_product(user_input)
+        end
+      end
+    end
+
+    private
+
+    def find_and_add_product(user_input)
+      uuid = user_input.to_i
+
+      matching_product = @products.find { |product| product.uuid == uuid }
+
+      return if matching_product.nil?
+
+      @cart.add_line_item(matching_product)
+    end
+
+    def print_status
+      puts
       print_instructions
       print_available_products
       print_cart
     end
-
-    private
 
     def print_instructions
       puts 'Instructions:'
